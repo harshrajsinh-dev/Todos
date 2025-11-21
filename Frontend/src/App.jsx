8f32a19fac634be1dddb54e14d69663deae550a8
harshrajsinh-dev <harshrajsinhgohil8626@gmail.com>
2025-11-21 19:28:07 +0530

    deleted unwanted packages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from './Pages/Login'
+import Home from './Pages/Home'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   )
 }
 
-export default App  
+export default App
